6dbff7a7e6e5ef57e568b46f99fe9889dfa5e363
Abdellatif <boudikerde1@gmail.com>
2022-12-07 18:21:51 +0200

    modified the js-basics-01-step-4

--- javascript-basics-01/step_4.js
+++ javascript-basics-01/step_4.js
@@ -1,7 +1,13 @@
 
-let Name = prompt("enter your name")
-let surname =prompt ("enter your last name")
-let city = prompt("enter your city ")
-alert("Hello "+ Name + " " +surname+ " " + "you are living in " + city);
-
-
+// let Name = prompt("enter your name")
+// let surname =prompt ("enter your last name")
+// let city = prompt("enter your city ")
+// alert("Hello "+ Name + " " +surname+ " " + "you are living in " + city);
+// document.getElementById("validate").onclick=function() {}
+function yahya(){
+  let n = document.getElementById("name").value;
+  let s = document.getElementById("surname").value;
+  let c =document.getElementById("city").value;
+  alert("your name is: "+n+" "+s+ " and you are living in "+c);
+}
+document.getElementById("validate").onclick=function() {yahya()};
