5c824a93b2559da18ecc8ad10b22a239ac46df8e
Abdellatif <boudikerde1@gmail.com>
2022-12-07 14:54:19 +0200

    js-basiscs-step_4 completed

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,7 @@
+
+let Name = prompt("enter your name")
+let surname =prompt ("enter your last name")
+let city = prompt("enter your city ")
+alert("Hello "+ Name + " " +surname+ " " + "you are living in " + city);
+
+
