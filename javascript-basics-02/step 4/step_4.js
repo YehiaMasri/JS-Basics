46e764fb70fc2d88ac420d5f1201deda70ac626d
Abdellatif <boudikerde1@gmail.com>
2022-12-07 21:12:43 +0200

    js-basics-02-step_4 completed

---
+++ javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,10 @@
+
+function reset() {
+  var a = confirm("Are you sure you want to reset all text?");
+  if (a == true) {
+       document.getElementById("name").value = "";
+       document.getElementById("surname").value = "";
+       document.getElementById("city").value = "";      
+  }
+} 
+document.querySelector("button").onclick=function(){reset()}
