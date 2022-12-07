d37745477739339aab271049d1a9ebfcc3145e5a
Abdellatif <boudikerde1@gmail.com>
2022-12-07 15:43:43 +0200

    js-basics-01-step_6

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,8 @@
+
+function remider_divider(){
+let a = document.getElementById("first_number").value
+let b=  document.getElementById("second_number").value
+let x=a/b;
+let y=a%b;
+alert("the divide of this two num: "+x+" and the remider is : "+y)
+}
