84e97e1f11d4d78cc36c28bd8607f75975be78f2
Abdellatif <boudikerde1@gmail.com>
2022-12-07 16:28:43 +0200

    js-basics-01-step-7 completed

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,6 @@
+function operater(){
+  let x=document.getElementById("shoe_size").value;
+  let y=document.getElementById("year").value;
+  let z= (((x*2)+5)*50)-y+1766;
+  alert("the number is: "+ z);
+}
