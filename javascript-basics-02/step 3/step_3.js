9ea22f1c5554d929a88dc56defe34c6170c35643
Abdellatif <boudikerde1@gmail.com>
2022-12-08 11:54:16 +0200

    modified js-basics-02-step_3

--- javascript-basics-02/step 3/step_3.js
+++ javascript-basics-02/step 3/step_3.js
@@ -2,4 +2,4 @@ function change_text(){
   let o = document.getElementById("name").value;
   document.getElementById("text").innerHTML=o;
 }
-let p =document.querySelector("#name").onchange=function(){change_text()};
+document.querySelector("#name").oninput=function(){change_text()};
